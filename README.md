b78137152a1b68da9cc797a18223f755b57c0d84
zarlashta manana <zarlashta27@yahoo.com>
2018-06-01 00:39:56 -0400

    Word-Guess-Game Homework Completed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Word-Guess-Game
+
+Hangman game in JavaScript
+
+This is the classic Hangman game where the player is given a word and has 12 chances to guess the word one letter at a time.
+
+Game Interface:
+
+Blank spaces are displayed on the screen equal to the length of the word chosen randomly from a list. Player can guess a letter by pressing a keyboard key. If the letter is present in the word, the blank spaces are replaced with the letter at the right position. If the letter occurs more than once in the word, each occurence of the letter is replaced for a correct guess.
+
+A letter can be guessed only once and for each wrong guess, the chances are decreased by 1. A new word is chosen after a successful game or after 12 guesses are up. 
+
+ The goal of this project is to learn more about object-oriented programming and become more comfortable using jQuery to build the front end.
+
+To try it out, open index.html in your browser of choice.
+
+Thanks and enjoy!
---
+++ assets/js/game.js
@@ -0,0 +1,148 @@
+//Made the variables first to use in the functions
+//Array of words
+var fruitWords = ['banana', 'fig', 'watermelon', 'grape', 'coconut', 'cherry', 'lime', 'pineapple'];
+
+var blanksAndSuccess = []; //Correct guesses and blanks will be in this array
+var blanks = 0; //holds blanks spaces for the word chosen
+var currentWord = "";
+var currentLetters = [];
+// Counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 12;
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+//array for users guessed letters
+var guessedLetter = [];
+//array for users correct guesses
+var correctLetter = [];
+//guessed letters that are incorrect
+var incorrectGuess = [];
+
+
+// create alphabet ul
+var buttons = function() {
+  //gets the alphabet button id from html
+  var myButtons = document.getElementById('alphabet-btns');
+  //creates unordered list for the letters
+  var letters = document.createElement('ul');
+
+
+  //loops through the alphabet
+  for (var i = 0; i < alphabet.length; i++) {
+    //creates li for list
+    let listItem = document.createElement('li');
+    //this creates the button and adds the bootstrap button look to it
+    listItem = document.createElement('BUTTON');
+    listItem.classList.add('btn-primary');
+    //gives each list item the id letter
+    //      list.id = 'letter';
+    listItem.innerHTML = alphabet[i];
+    //appends listIem to my buttons
+    myButtons.appendChild(listItem);
+    listItem.dataset.alphabet = alphabet[i];
+
+
+    listItem.onclick = function() {
+      var userGuess = listItem.dataset.alphabet;     
+      guessedLetter.push(userGuess);
+      document.getElementById('guessed').innerHTML = "Letters Already Guessed: " + guessedLetter.join(" ");
+      checkLetters(userGuess); // runs the code to check for correctness      
+      round();
+    }
+
+  }
+}
+
+document.getElementById("gameStart").onclick = function() {buttons()};
+
+
+//Below is the functionality of the game
+function startGame() {
+  numGuesses = 12;
+  blanksAndSuccess = [];
+  guessedLetter = [];
+  incorrectGuess = [];
+  //Selects a fruitword at random
+  currentWord = fruitWords[Math.floor(Math.random() * fruitWords.length)];
+
+  //Splits the current word into letters, so that you can match the users guesses to the letters of the word
+  currentLetters = currentWord.split("");
+
+  //Need to know how many blanks
+  blanks = currentLetters.length;
+  for (var i = 0; i < blanks; i++) {
+    blanksAndSuccess.push("_")
+  }
+  console.log(currentWord);
+  document.getElementById('currentWord').innerHTML = "Find the missing letters: " + blanksAndSuccess.join(" ");
+  document.getElementById('guessesRemaining').innerHTML = "Guesses left: " + numGuesses;
+  document.getElementById('guessed').innerHTML = "Letters already guessed: "
+}
+
+//Checks if users letter is in the word
+function checkLetters(letter) {
+  //using Boolean to check if the letter is in the word
+  var letterInWord = false;
+  //loop that goes through the length of the word
+  for (var i = 0; i < blanks; i++) {
+    if (currentWord[i] == letter) {
+      letterInWord = true;
+    }
+  }
+  if (letterInWord) {
+    for (var i = 0; i < blanks; i++) {
+
+      if (currentWord[i] == letter) {
+        blanksAndSuccess[i] = letter
+      }
+    }
+    console.log(blanksAndSuccess);
+
+  } else {
+    incorrectGuess.push(letter);
+    numGuesses--;
+    console.log("that was incorrect " + numGuesses + " are remaining");
+  }
+}
+
+//Upon finishing
+function round() {
+
+  console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+
+  // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
+  document.getElementById("guessesRemaining").innerHTML = "Number Of Guesses Remaining: " + numGuesses;
+  document.getElementById("currentWord").innerHTML = "Find the missing letters: " + blanksAndSuccess.join(" ");
+  document.getElementById("guessed").innerHTML = "Letters already guessed: " + incorrectGuess.join(" ");
+
+  // If we have gotten all the letters to match the solution... 
+  if (currentLetters.toString() == blanksAndSuccess.toString()) {
+    winCounter++; // add to the win counter 
+    document.getElementById("word").innerHTML = "The last word was " + currentWord;
+    alert("You win! The word was " + currentWord); // give the user an alert   
+
+    // Update the win counter in the HTML
+    document.getElementById("winCounter").innerHTML = "You have won " + winCounter + " game(s)";
+    startGame(); // restart the game 
+  }
+
+  // If we've run out of guesses
+  else if (numGuesses == 0) {
+    lossCounter++; // add to the loss counter 
+    document.getElementById("word").innerHTML = "The last word was " + currentWord;
+
+    alert("You lose. The word was " + currentWord); // give the user an alert
+
+    // Update the loss counter in the HTML
+    document.getElementById("lossCounter").innerHTML = "You have lost " + lossCounter + " game(s)";
+    startGame(); // restart the game
+  }
+}
+
+//Making the stuff run
+//Calling the startGame function
+startGame();
